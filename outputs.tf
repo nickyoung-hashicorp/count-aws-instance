3e1087760a2f708f9f22038b319d0ca8addf71c2
nickyoung-hashicorp <nyoung@hashicorp.com>
2022-05-19 08:49:58 -0700

    changes

--- outputs.tf
+++ outputs.tf
@@ -1,14 +1,4 @@
 # SSH to Vault instance
-output "vault_ssh" {
-  value = "ssh -i ssh-key.pem ubuntu@${aws_eip.vault.public_ip}"
-}
-
-# Vault IP
-output "vault_ip" {
-  value = aws_eip.vault.public_ip
-}
-
-# Output RDS address
-output "rds-address" {
-  value = aws_db_instance.rds.address
+output "aws-instances" {
+  value = aws_instance.server.tags_all
 }
