5bc6f42d0da37a8629b23e19dc719f66d1946cb4
nickyoung-hashicorp <nyoung@hashicorp.com>
2022-05-19 09:40:14 -0700

    changes

--- outputs.tf
+++ outputs.tf
@@ -1,8 +1,4 @@
 # SSH to Vault instance
-output "public-ips" {
-  value = aws_instance.server.*.public_ip
-}
-
-output "tags" {
-  value = aws_instance.server.*.tags_all
+output "public-ips-and-tags" {
+  value = "${aws_instance.server.*.public_ip} \n ${aws_instance.server.*.tags_all}"
 }
